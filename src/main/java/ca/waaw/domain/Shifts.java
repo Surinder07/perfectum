dd19a7cb0f1011a8f3d8671a6ecf2650b3ab6273
Akhil Singh Chauhan <akhil@bulsoft.com>
2022-08-24 16:22:26 +0530

    Shifts + Recurring Shifts entities and repository created

---
+++ src/main/java/ca/waaw/domain/Shifts.java
@@ -0,0 +1,31 @@
+package ca.waaw.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.Instant;
+
+@Data
+@Entity
+@Table(name = "shifts")
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class Shifts extends AbstractEntity {
+
+    @Column(name = "user_id")
+    private String userId;
+
+    private Instant start;
+
+    private Instant end;
+
+    private String notes;
+
+    @Column(name = "location_role_id")
+    private String locationRoleId;
+
+}
