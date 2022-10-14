11c2988dddb50458c2ca64d3ed1d94ca1d6b3bbf
Akhil Singh Chauhan <akhil@bulsoft.com>
2022-10-14 20:17:51 +0530

    time_offs: Entity and Dtos designed

--- src/main/java/ca/waaw/domain/TimeOffs.java
+++ src/main/java/ca/waaw/domain/TimeOffs.java
@@ -5,26 +5,23 @@ import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.time.Instant;
 
 @Data
 @Entity
 @Table(name = "time_offs")
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
+@NamedQuery(name = "TimeOffs.getByUserIdBetweenDates", query = "SELECT to FROM TimeOffs to WHERE" +
+        "to.userId = ?1 AND (to.startDate IS BETWEEN ?2 AND ?3 OR to.endDate IS BETWEEN ?2 AND ?3)")
 public class TimeOffs extends AbstractEntity {
 
-    // startDate
+    private Instant startDate;
 
-    // endDate
+    private Instant endDate;
 
-    // notes
+    private String note;
 
-    // startTime
-
-    // endTime
-
-    // userId
-
-    // timeOffType
+    private String userId;
 
 }
--- src/main/java/ca/waaw/repository/TimeOffsRepository.java
+++ src/main/java/ca/waaw/repository/TimeOffsRepository.java
@@ -4,6 +4,14 @@ import ca.waaw.domain.TimeOffs;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
+import java.util.Optional;
+
 @Repository
 public interface TimeOffsRepository extends JpaRepository<TimeOffs, String> {
+
+    Optional<TimeOffs> getByUserIdBetweenDates(String userId, Instant start, Instant end);
+
+    Optional<TimeOffs> findOneByIdAndDeleteFlag(String id, boolean deleteFlag);
+
 }
