b60fb9265ccd10c00690f6f91f2dab9a469893bc
Akhil Singh Chauhan <akhil@bulsoft.com>
2022-10-18 17:28:38 +0530

    Missing comments for mappers added

--- src/main/java/ca/waaw/mapper/LocationRoleMapper.java
+++ src/main/java/ca/waaw/mapper/LocationRoleMapper.java
@@ -18,6 +18,10 @@ import java.util.List;
 
 public class LocationRoleMapper {
 
+    /**
+     * @param source location role entity
+     * @return location role dto with all info
+     */
     public static LocationRoleDto entityToDto(LocationRole source) {
         LocationRoleDto target = new LocationRoleDto();
         BeanUtils.copyProperties(source, target);
@@ -26,10 +30,15 @@ public class LocationRoleMapper {
         return target;
     }
 
+    /**
+     * @param source1 locationRole info
+     * @param source2 loggedIn user info
+     * @return Location role entity to be saved in database
+     */
     public static LocationRole dtoToEntity(LocationRoleDto source1, User source2) {
         LocationRole target = new LocationRole();
         BeanUtils.copyProperties(source1, target);
-        target.setLocationId(source2.getAuthority().equals(Authority.ADMIN)? source1.getLocationId() : null);
+        target.setLocationId(source2.getAuthority().equals(Authority.ADMIN) ? source1.getLocationId() : null);
         target.setOrganizationId(source2.getOrganizationId());
         target.setCreatedBy(source2.getId());
         target.setStatus(EntityStatus.ACTIVE);
@@ -38,6 +47,10 @@ public class LocationRoleMapper {
         return target;
     }
 
+    /**
+     * @param source location role with user entity
+     * @return detailed location role with user dto
+     */
     public static LocationRoleWithUsersDto entityToDto(LocationRolesUser source) {
         LocationRoleWithUsersDto target = new LocationRoleWithUsersDto();
         BeanUtils.copyProperties(source, target);
@@ -51,21 +64,35 @@ public class LocationRoleMapper {
         return target;
     }
 
+    /**
+     * @param source location role entity
+     * @return location role dto
+     */
     public static BaseLocationRole entityToDtoSimple(LocationRole source) {
         BaseLocationRole target = new BaseLocationRole();
         BeanUtils.copyProperties(source, target);
         return target;
     }
 
+    /**
+     * @param source dto with location role info
+     * @param target entity in which info will be updated
+     */
     public static void updateDtoToEntity(LocationRoleDto source, LocationRole target) {
         if (source.getIsTimeoffEnabled() != null) target.setTimeoffEnabled(source.getIsTimeoffEnabled());
         if (source.getIsTimeclockEnabled() != null) target.setTimeclockEnabled(source.getIsTimeclockEnabled());
         if (source.getTotalHoursPerDayMin() != 0) target.setTotalHoursPerDayMin(source.getTotalHoursPerDayMin());
         if (source.getTotalHoursPerDayMax() != 0) target.setTotalHoursPerDayMax(source.getTotalHoursPerDayMax());
         if (source.getMinHoursBetweenShifts() != 0) target.setMinHoursBetweenShifts(source.getMinHoursBetweenShifts());
-        if (source.getMaxConsecutiveWorkDays() != 0) target.setMaxConsecutiveWorkDays(source.getMaxConsecutiveWorkDays());
+        if (source.getMaxConsecutiveWorkDays() != 0)
+            target.setMaxConsecutiveWorkDays(source.getMaxConsecutiveWorkDays());
     }
 
+    /**
+     * @param locationSource     location entity
+     * @param locationRoleSource location role entity
+     * @return location and location role dto info with minimal details
+     */
     public static LocationAndRoleDto locationEntityToDetailDto(Location locationSource, LocationRole locationRoleSource) {
         LocationAndRoleDto locationAndRoleInfo = new LocationAndRoleDto();
         locationAndRoleInfo.setLocationId(locationSource.getId());
