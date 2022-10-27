663834761106e3d996679ffbe82b4aa16d2e1d5c
Akhil Singh Chauhan <akhil@bulsoft.com>
2022-10-27 10:54:28 +0530

    Report service file added

---
+++ src/main/java/ca/waaw/service/ReportsService.java
@@ -0,0 +1,18 @@
+package ca.waaw.service;
+
+import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ReportsService {
+
+    private final Logger log = LogManager.getLogger(ReportsService.class);
+
+    // Generate payroll report
+
+    // Generate Attendance report
+
+}
