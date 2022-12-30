3fd860c25339683be75f8803fea3806db01b0d21
Akhil Singh Chauhan <akhil@bulsoft.com>
2022-12-30 21:34:36 +0530

    Shift query fixes

--- src/main/java/ca/waaw/domain/joined/ShiftDetails.java
+++ src/main/java/ca/waaw/domain/joined/ShiftDetails.java
@@ -20,14 +20,21 @@ import java.time.Instant;
 @Table(name = "shifts")
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
-@NamedQuery(name = "ShiftDetails.searchAndFilterShifts", query = "SELECT s from ShiftDetails s LEFT JOIN FETCH s.user u " +
-        "LEFT JOIN FETCH s.location l LEFT JOIN FETCH s.locationRole r  " +
-        "WHERE (?1 IS NULL OR (u.firstName LIKE CONCAT('%', ?1, '%') OR u.lastName LIKE CONCAT('%', ?1, '%') OR " +
-        "u.email LIKE CONCAT('%', ?1, '%'))) AND (?2 IS NULL OR l.id = ?2) AND (?3 IS NULL OR " +
-        "r.id = ?3) AND (?4 IS NULL OR s.shiftStatus = ?4) AND (?5 IS NULL OR u.id = ?5)" +
-        "AND (?6 = TRUE OR r.adminRights = FALSE) AND s.deleteFlag = FALSE " +
-        "AND ((?7 IS NULL OR ?8 IS NULL) OR (b.startDate BETWEEN ?7 AND ?8 OR b.endDate " +
-        "BETWEEN ?7 AND ?8))")
+@NamedQueries({
+        @NamedQuery(name = "ShiftDetails.searchAndFilterShifts", query = "SELECT s from ShiftDetails s LEFT JOIN FETCH s.user u " +
+                "LEFT JOIN FETCH s.location l LEFT JOIN FETCH s.locationRole r  " +
+                "WHERE (?1 IS NULL OR (u.firstName LIKE CONCAT('%', ?1, '%') OR u.lastName LIKE CONCAT('%', ?1, '%') OR " +
+                "u.email LIKE CONCAT('%', ?1, '%'))) AND (?2 IS NULL OR l.id = ?2) AND (?3 IS NULL OR " +
+                "r.id = ?3) AND (?4 IS NULL OR s.shiftStatus = ?4) AND (?5 IS NULL OR u.id = ?5)" +
+                "AND (?6 = TRUE OR r.adminRights = FALSE) AND s.deleteFlag = FALSE "),
+        @NamedQuery(name = "ShiftDetails.searchAndFilterShiftsDate", query = "SELECT s from ShiftDetails s LEFT JOIN FETCH s.user u " +
+                "LEFT JOIN FETCH s.location l LEFT JOIN FETCH s.locationRole r  " +
+                "WHERE (?1 IS NULL OR (u.firstName LIKE CONCAT('%', ?1, '%') OR u.lastName LIKE CONCAT('%', ?1, '%') OR " +
+                "u.email LIKE CONCAT('%', ?1, '%'))) AND (?2 IS NULL OR l.id = ?2) AND (?3 IS NULL OR " +
+                "r.id = ?3) AND (?4 IS NULL OR s.shiftStatus = ?4) AND (?5 IS NULL OR u.id = ?5)" +
+                "AND (?6 = TRUE OR r.adminRights = FALSE) AND s.deleteFlag = FALSE " +
+                "AND ((?7 IS NULL OR ?8 IS NULL) OR (s.start BETWEEN ?7 AND ?8 OR s.end BETWEEN ?7 AND ?8))")
+})
 public class ShiftDetails extends AbstractEntity {
 
     @OneToOne(fetch = FetchType.LAZY)
